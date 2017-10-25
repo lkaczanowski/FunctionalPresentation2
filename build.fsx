408afb17874684df48806a757472d20575f765e2
Lucas Kaczanowski <lucas.kaczanowski@gmail.com>
2017-10-25 09:37:25 +0200

    Change projectName

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@
 let gitOwner = "lkaczanowski"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "FunctionalPresentation"
+let gitProjectName = "FunctionalPresentation2"
 // The name of the GitHub repo subdirectory to publish slides to
 let gitSubDir = ""
 
